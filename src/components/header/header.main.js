5deeffae2d5123df4bf85fbece3c6456a279d892
Joey Robinson <theamazingness@gmail.com>
2021-04-13 07:25:44 -0400

    we're going to use both hehe xd

--- src/components/header/header.main.js
+++ src/components/header/header.main.js
@@ -1,7 +1,7 @@
 import Navigation from "./header.nav"
 
 const Header = () => (
-  <header>
+  <header className="bg-lightIndie h-20 grid items-center justify-items-end">
     <Navigation />
   </header>
 )
