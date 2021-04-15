11f6772a07bf8b68f4f1d86d52c95158eb6ddaaa
Joey Robinson <theamazingness@gmail.com>
2021-04-14 20:25:22 -0400

    re-added tailwind as that will be primary styling and scss will be for more granular styling

--- src/components/header/header.main.js
+++ src/components/header/header.main.js
@@ -1,7 +1,7 @@
 import Navigation from "./header.nav"
 
 const Header = () => (
-  <header className="bg-lightIndie h-20 grid items-center justify-items-end">
+  <header className="bg-lightIndie h-14 grid items-center justify-items-end">
     <Navigation />
   </header>
 )
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-import "tailwindcss/tailwind.css"
 import "../../styles/main.scss"
+import "../../styles/tailwind.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
