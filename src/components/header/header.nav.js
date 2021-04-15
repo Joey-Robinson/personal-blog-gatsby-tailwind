2930041e6c32a94e4bb3c90fc0ab86d5e0429b4a
Joey Robinson <theamazingness@gmail.com>
2021-04-14 20:02:58 -0400

    updating animation for nav

--- src/components/header/header.nav.js
+++ src/components/header/header.nav.js
@@ -1,26 +1,22 @@
 import Link from "next/link"
-import { useRouter } from "next/router"
 
 const Navigation = () => {
-  const router = useRouter()
   return (
     <nav>
       <ul>
         <li>
-          <Link href="/" className="hover">
-            <a className={router.pathname == "/" ? "active" : ""}>Home</a>
+          <Link href="/">
+            <a>Home</a>
           </Link>
         </li>
         <li>
           <Link href="/blog">
-            <a className={router.pathname == "/blog" ? "active" : ""}>Blog</a>
+            <a>Blog</a>
           </Link>
         </li>
         <li>
           <Link href="/contact">
-            <a className={router.pathname == "/contact" ? "active" : ""}>
-              Contact
-            </a>
+            <a>Contact</a>
           </Link>
         </li>
       </ul>
